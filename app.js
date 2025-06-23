a2292e6fa8b559fbed7383bf1201038c43153ac7
Barcelarzao1995 <paulog161@gmail.com>
2025-06-23 18:12:34 -0300

    Atualizações e novas rotas adicionadas

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ import transacoesRoutes from './routes/transacoes.js';
 import syncRoutes from './routes/sync.js';
 import authRoutes from './routes/auth.js';
 import notaRoutes from './routes/nota.js'; // ✅ adicionando notas
+import cartoesRoutes from './routes/cartoes.js';
+import devedoresRoutes from './routes/devedores.js';
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use('/api/transacoes', transacoesRoutes);
 app.use('/api/sync', syncRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/notas', notaRoutes); // ✅ rota nova
+app.use('/cartoes', cartoesRoutes);
+app.use('/devedores', devedoresRoutes);
 
 app.get('/', (req, res) => {
   res.send('🚀 API conectada com MongoDB');
