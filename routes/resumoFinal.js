7ec70cf5645d6a19a28cc3f419965c34e8f650bc
Barcelarzao1995 <paulog161@gmail.com>
2025-07-01 01:10:00 -0300

    Atualizações e novas rotas adicionadas

--- routes/resumoFinal.js
+++ routes/resumoFinal.js
@@ -1,11 +1,11 @@
 import express from 'express';
-import { getResumoFinal } from '../controllers/resumoFinalController.js';
+import { getResumoFinanceiro } from '../controllers/resumoFinalController.js';
 import { exportarResumoExcel } from '../controllers/resumoFinalExcelController.js';
 import { autenticarToken } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/', autenticarToken, getResumoFinal);
+router.get('/', autenticarToken, getResumoFinanceiro);
 router.get('/excel', autenticarToken, exportarResumoExcel);
 
 export default router;
